2920962f17cfcd8fa15f9a34ca61218389a1b5fe
ru5s <hellperock@gmail.com>
2022-08-12 09:38:41 +0500

    Initial Commit

---
+++ Viper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Viper
+//
+//  Created by Ruslan Ismailov on 12/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
